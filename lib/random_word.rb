1a07c8eb79a0b88b4e79216c84079cda4a12793a
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-28 22:30:22 +0100

    Setup random selection from 10000 word txt file.

--- lib/random_word.rb
+++ lib/random_word.rb
@@ -1,1 +1,27 @@
 # random_word.rb
+# frozen_string_literal: true
+
+class RandomWord
+  def initialize
+    @random_word_file = File.readlines('google-10000-english-no-swears.txt')
+    @random_word = select_random_word
+  end
+
+  def select_random_word
+    array_random_index = Array(0..10_000)
+
+    loop do
+      random_index = array_random_index.sample
+      random_word = @random_word_file[random_index]
+
+      if random_word.length.nil?
+        select_random_word
+      elsif random_word.length > 4 && random_word.length <= 12
+        puts "#{random_word}#{random_index}"
+        return random_word
+      end
+    end
+  end
+end
+
+word = RandomWord.new
