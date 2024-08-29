b09abca40e2726f2111567b139f68050624e97f3
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-29 13:29:39 +0100

    Allow player guess input.

--- lib/player_guess.rb
+++ lib/player_guess.rb
@@ -1,2 +1,15 @@
 # player_guess.rb
+# frozen_string_literal: true
 
+class PlayerGuess
+  def initialize
+    @player_guess = player_guess
+  end
+
+  def player_guess
+    print 'Input word: '
+    gets.chomp.downcase
+  end
+end
+
+word = PlayerGuess.new
