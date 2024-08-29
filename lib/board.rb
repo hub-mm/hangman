27a2154689f3e720c940f461adfcea2806ac50b4
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-29 14:36:56 +0100

    Rework method of showing hangman, more applicable.

--- lib/board.rb
+++ lib/board.rb
@@ -2,70 +2,218 @@
 # frozen_string_literal: false
 
 class Board
-  def initialize
-    @board = display_board
-  end
-
   def board_top
-    top = '___________'
+    '___________'
   end
 
   def board_column
-    column = ' |'
+    ' |'
   end
 
   def board_bottom
-    bottom = '------'
+    '------'
   end
 
   def board_rope
-    rope = '       |'
+    '       |'
   end
 
   def board_head
-    head = '       O'
+    '       O'
   end
 
   def board_shoulders
-    shoulders = '      -+-'
+    '      -+-'
   end
 
   def board_chest_top
-    chest = ' | '
+    '       | '
   end
 
-  def board_chest_botom
-    chest = '       -'
+  def board_chest_bottom
+    '       -'
   end
 
   def board_arm_left
-    arm_left = '     /'
+    '     / '
   end
 
   def board_arm_right
-    arm_right = '\\'
+    ' \\'
   end
 
   def board_leg_left
-    leg_left = '     _/ '
+    '     _/ '
   end
 
   def board_leg_right
-    leg_right = '\\_'
+    '\\_'
   end
 
-  def display_board
+  def board_head_x
+    '       X'
+  end
+
+  def display_bottom
+    puts board_bottom
+  end
+
+  def display_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_top
+    puts board_top
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_rope
+    puts board_top
+    puts board_column << board_rope
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_head
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_shoulders
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head
+    puts board_column << board_shoulders
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_chest
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head
+    puts board_column << board_shoulders
+    print board_column
+    puts board_chest_top
+    puts board_column << board_chest_bottom
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_arm_left
     puts board_top
     puts board_column << board_rope
     puts board_column << board_head
     puts board_column << board_shoulders
     print board_column << board_arm_left
-    print board_chest_top
+    puts board_chest_top.delete(' ')
+    puts board_column << board_chest_bottom
+    puts board_column
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_arm_right
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head
+    puts board_column << board_shoulders
+    print board_column << board_arm_left
+    print board_chest_top.delete(' ')
     puts board_arm_right
-    puts board_column << board_chest_botom
+    puts board_column << board_chest_bottom
+    puts board_column
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_leg_left
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head
+    puts board_column << board_shoulders
+    print board_column << board_arm_left
+    print board_chest_top.delete(' ')
+    puts board_arm_right
+    puts board_column << board_chest_bottom
+    puts board_column << board_leg_left
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_leg_right
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head
+    puts board_column << board_shoulders
+    print board_column << board_arm_left
+    print board_chest_top.delete(' ')
+    puts board_arm_right
+    puts board_column << board_chest_bottom
+    print board_column << board_leg_left
+    puts board_leg_right
+    puts board_column
+    puts board_bottom
+  end
+
+  def display_game_over
+    puts board_top
+    puts board_column << board_rope
+    puts board_column << board_head_x
+    puts board_column << board_shoulders
+    print board_column << board_arm_left
+    print board_chest_top.delete(' ')
+    puts board_arm_right
+    puts board_column << board_chest_bottom
     print board_column << board_leg_left
     puts board_leg_right
     puts board_column
     puts board_bottom
   end
 end
+
+# Test/Troubleshoot
+show = Board.new
+show.display_bottom
+show.display_column
+show.display_top
+show.display_rope
+show.display_head
+show.display_shoulders
+show.display_chest
+show.display_arm_left
+show.display_arm_right
+show.display_leg_left
+show.display_leg_right
+show.display_game_over
